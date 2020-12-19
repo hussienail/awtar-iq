5d0250345d5fa4c0202fd3aa69ad32e3d25c36ee
hussienail <63175073+hussienail@users.noreply.github.com>
2020-12-19 19:02:10 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,68 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+  
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="sico/z1eojuqf3m10.jpg" >
+<a id="jfa44"target="_blank"type="a"rel="nofollow"href=""class="label-primary">1 </a></div></div><center> <a id="asim99" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a>
+</div></div> <div id="asim1"><a id="asim2"target="_blank"type="a"href="rules.html"style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;"class="btn minix btn-primary">2</a>
+<a id="asim2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">3</a>
+<a id="asim10"target="_blank"type="a"href=""style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">4</a>
+<a id="asim10"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">5</a>
+<a id="asim10"target="_blank"type="a"href=" "style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">6</a>
+<a id="asim10"target="_blank"type="a""style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">7</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">عاصم68</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">عاصم98</a>
+<a id="hack2"target="_blank"type="a"href=""style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">عاصم8</a>
+<a id="hack2"target="_blank"type="a"href="#"style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">عاصم7<center>
+`).insertBefore(".nav-tabs")
+$(".ae.label.label-primary.fa.fa-gear").text(عاصم6); 
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="70%" style="">`)
+$(`<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3">1<font color="#595757" class="jfa99">2</font>3 <font class="jfa99" color="#727A54">32</font>4<font class="jfa88" color="#ff9900;>66</font> 5<font class="jfa88" color="#ff00ff">88</font>77<font class="jfa88" color="#5CAD9A">12</font> 99<font class="jfa88" color="#800000>65</font>00<font class="jfa88" color="#417570">54</font>09</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'); 
+$(`<link rel="stylesheet" href="https://alhaded-com.000webhostapp.com/Master/master.css"></style>`).insertBefore("body")
+
+$(".dpnl").css('width','310px'); 
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() { alert(asimmmm); 
+
+$ ( ` <center> <div style =" width: 100?ط› height: 0%! importantط› "id =" as "margin: 2px" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> 
+` ). insertBefore ( '#d2' );
+$(`<label onclick="window.open('cp?token='+token,'_blank');" style="background-color: ghostwhite;color: black;margin:4px; padding:8px;width:98%;" class="label tc border cp  btn  label-danger fl"><span class="fl fa fa-star"></span> عاصم5</label>`).insertAfter('#settings label.label.tc.border.btn.fl[onclick="sendpic();"]');
+$('.u-msg.dots').removeClass('dots');
+Tsend_=Tsend;reqlikes=5;Tsend=function(){if(getuser(myid).rep<reqlikes){alert('عاصم3 ');return;}else{Tsend_();}}
+setprofile_=setprofile;reqlikes=5;setprofile=function(){if(getuser(myid).rep<reqlikes){alert('عاصم4');return;}else{setprofile_();}}
+$(".ubnr").remove();
+
+$("div#tlogins button.btn.btn-primary")
+    .click(function () {
+        var b = setInterval(function () {
+            var c = $(".pmsgc")
+                .length;
+            0 < c ? ($(` </div><center>
+
+`)
+                .appendTo("#mic"), clearInterval(b)) : console.log(c)
+        }, 1)
+    }), $("div#tlogins button.btn.btn-primary")
+    .click(function () {
+        setTimeout(function () {
+            refreshonline()
+        }, 0)
+    }), setInterval(function () {
+        var a = $("#dpnl");
+        0 < a.length && a.remove()
+    }, 0), $(".dpnl")
+    .append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`), $('#d0')
+    .append(`<label title="Omar  - Ranea"href="#"onclick="$('.pnhead').text($(this).attr('title'));
+hl($(this),'primary');
+setTimeout(function()
+	{
+	$('#mic').scrollTop(0);
+}
+,0);
+$('.dpnl').show();
+"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-volume-up">&nbsp;</label>`);
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>عاصم2 </span></a>-->
+<a href="" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >عاصم1¯</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+AddMsg_=AddMsg;AddMsg=function(wid,data){data.msg=data.msg.replace(/\u102C/g,"");return AddMsg_(wid,data);}
+
+$('.fr.borderg a').text(' برمجه دوك موبايل¯ ')
