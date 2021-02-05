238a48a757af8a0294c86d54025dbad263c4fc5d
hussienail <63175073+hussienail@users.noreply.github.com>
2021-02-05 17:10:10 +0300

    Update and rename ع8.js to ع9.js

--- ع9.js
+++ ع9.js
@@ -1,4 +1,4 @@
-$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://www.up-00.com/i/00202/pms5ptcpk7hp.jpg
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://www.up-00.com/i/00202/wiccp7o82psg.jpg
 " > <center>
  <a id="asim99" href="" class="fl mini  fa   fa fa-refresh btn btn-success"></a></div></div>
 <div id="asim1"><a id="asim2"target="_blank"type="a"href="rules.html"style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;"class="btn minix btn-primary">القوانين</a><a id="asim2"target="_blank"type="a"href="sub.html"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">الاشتراكات</a><a id="asim2"target="_blank"type="a"href="/"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">نجمه الحب</a><a
