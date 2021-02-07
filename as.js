648898e86369008609d41bf45f38a0397f6e8d9b
hussienail <63175073+hussienail@users.noreply.github.com>
2021-02-07 19:35:05 +0300

    Create as.js

---
+++ as.js
@@ -0,0 +1,73 @@
+					</a><button class="btn fr btn-primary fa fa-refresh" style="padding: 3px 4px;margin:0px;border: 3px solid;border-radius: 50%;margin-left: -50px;color: #fff;background-image: none;" ></button>
+					<div class="rainbowo"></div>
+					<label class="primaryborder label mini fl" style="margin-bottom: 0;width:100%;border-radius:0px;padding: 1px 0 1px;border-bottom: 1px solid;border-top: 1px solid;margin-top: 1px;">
+						<a href="#" target="_blank" style="width: 22%;padding: 0px 2px 1px;border: none;" type="a" class="primaryColor btn btn22">
+							<i class="fa fa-wifi" aria-hidden="true" style="margin: 2px 2px 0;float: left;"></i>
+
+							اتصل بنا
+						</a>
+						<a href="#" target="_blank" style="width: 21%;margin: 0 3px;" type="a" class="primaryColor btn btn22">
+							<i class="fa fa-mobile" aria-hidden="true" style="margin: 0 2px;float: left;font-size: 24px!important;"></i>
+
+							التطبيق
+						</a>
+						<a href="#" target="_blank" style="width: 27%;margin: 0 3px;" type="a" class="primaryColor btn btn22">
+							<i class="fa fa-newspaper-o" aria-hidden="true"  style="margin: 2px 2px 0;float: left;"></i>
+
+							قوانين الموقع
+						</a>
+						<a href="#" target="_blank" style="width: 24%;" type="a" class="primaryColor btn btn22">
+							<i class="fa fa-star" aria-hidden="true" style="margin: 2px 2px 0;float: left;"></i>
+
+							ميز نفسك
+						</a>
+					
+					</label>
+
+					<div style="background-image: url(styleone/m3.png);width: 100%;background-size: cover;float: right;padding: 12px 2px 15px;">
+							<ul class="nav  nav-tabs label-primary" style="margin: 0 16px;border-radius: 10px;border: 2px solid #4e4d4c6b;border-bottom: none;"> 
+        <li class="active">
+									
+									<a class="bg" data-toggle="tab" style="padding: 0;" href="#l1">
+										<i class="btn-primary  fa fa-user"></i>
+										دخول الزوار
+									</a>
+								
+								</li>
+        <li style="width: 31.5%;margin-right: -5px;margin-left: 6px;">
+									<a class="bg" data-toggle="tab" style="padding: 0;" href="#l2">
+										<i class="btn-primary  fa fa-user"></i>
+										دخول الاعضاء
+									</a>
+								</li>
+        <li style="width: 34%;float: right;">
+									<a class="bg" data-toggle="tab" style="padding: 0;"  href="#l3">
+										<i style="padding: 4px 3px;" class="btn-primary fa fa-user-plus"></i>
+										تسجيل عضويه
+									</a></li>
+      </ul>
+
+						<div class="bg" style="border-radius: 0 0 10px 10px;float: left;margin: 0 40px;">
+							
+
+						<div style="padding: 10px;float: left;height: 120px;border-radius: 6px;box-shadow: 0px 5px 5px rgb(59, 57, 56), 0px 2px 1px rgb(0, 0, 0);">
+							
+							<div style="background-image: url(styleone/m1.png);background-size: cover;background-color: #89867f;width: 100%;height: 100%;border-radius: 6px;box-shadow: 3px 3px 3px rgb(59, 57, 56), 0 2px 2px rgb(0, 0, 0);padding: 0 40px;">
+      <div id="l1" style="padding:8px;width:100%;" class=" grow break tab-pane fade in active">
+        <input style="width: 100%;border: none;padding: 2px 3px;text-align: center;margin: 0 0 2px 0;"  class="border corner" class="border corner" id="u1" placeholder="أكتب الاسم المستعار">
+        <input style="width: 100%;opacity: 0;border: none;padding: 2px 3px;text-align: center;margin: 2px 0 5px;" disabled="disabled">
+        <button style="float: right;background-color: #ffffff!important;border: none;padding: 1px 10px;" onclick="login(1);" class="primaryColor btn btn-primary">دخول</button><br>&nbsp;
+      </div>
+      <div id="l2" style="padding:8px;width:100%;" class=" grow break tab-pane fade hid">
+        <input style="width: 100%;border: none;padding: 2px 3px;text-align: center;margin: 0 0 2px 0;" id="u2" class="border corner" placeholder="اكتب اسم العضو"><br>
+        <input style="width: 100%;border: none;padding: 2px 3px;text-align: center;margin: 2px 0 5px;" id="pass1" class="border corner" type="password" placeholder="اكتب كلمه المرور">
+
+        <button style="float: right;background-color: #ffffff!important;border: none;padding: 1px 10px;" onclick="login(2);" class="primaryColor btn btn-primary">دخول</button>
+                <div class="checkbox" style="margin:0px;display:inline;">
+          <label style="color:white"><input id="stealth" type="checkbox" value="">دخول مخفي</label>
+        </div>
+      </div>
+      <div id="l3" style="padding:8px;width:100%;" class=" grow break tab-pane fade hid">
+        <input style="width: 100%;border: none;padding: 2px 3px;text-align: center;margin: 0 0 2px 0;" id="u3" class="width: 100%;border corner" placeholder="اكتب اسم العضو"><br>
+        <input style="width: 100%;border: none;padding: 2px 3px;text-align: center;margin: 2px 0 5px;" id="pass2" class="border corner" type="password" placeholder="اكتب كلمه المرور">
+        <button style="float: right;background-color: #ffffff!important;border: none;padding: 1px 10px;" onclick="login(3);" class="primaryColor btn btn-primary">دخول</button>
