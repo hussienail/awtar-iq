a55ed1dcfee5b93da2ac5e886eeb25bbd4da10a0
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-12-28 01:00:54 +0300

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,96 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(` <center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;● · مـوسـيـقـى · ●&nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>اغاني</p><audio controls loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),
+$(`<img src ="https://c.top4top.io/p_18615n1.jpg" etclass="fr" style="width:100%;margin-top: 1px;"><div id="asim10"><center><a id="asim33" href="contact.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اتصل بنا</a><a id="asim33" href="mobile.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a><a id="asim33" href="Shortcut.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اختصارات</a><a id="asim33" href="style1.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">زخرفة</a><a id="asim33" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين<a id="asim33" href="ruless.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">المراقبين<a id="asim32" href="" target="_blank" type="button" style=" width: 18%; margin: 2px;" class="fa fa-viadeo btn btn-primary btn-sm"> المدونة<a id="asim31" href="" target="_blank" type="button" style=" width: 18%; margin: 2px;" class="fa fa-viadeo btn btn-primary btn-sm"> المنتدى<a id="asim29" href="" target="_blank" type="button" style=" width: 18%; margin: 2px;" class="fa fa-viadeo btn btn-primary btn-sm"> الجوهره<a id="asim32" href="" target="_blank" type="button" style=" width: 18%; margin: 2px;" class="fa fa-viadeo btn btn-primary btn-sm"> المدونة<a id="asim31" href="" target="_blank" $(`<style>
+.broadcasters>div>.prod {
+    float: right;
+    width: 30px;
+    height: 30px;
+    border: #c93091;
+    margin: 1px 11px 0 7px;
+    text-align: center;
+    border-radius: 23%;
+    color: #fff;
+    background-color: #c93091;
+    border-left: 2px solid #f7d322;
+    border-right: 2px solid #f7d322;
+}
+a.btn.minix.fa.fa-times.fr, a.btn.minix.fa.fa-thumbs-up.fr, a.btn.minix.fa.fa-comment.fr {
+    border-left: 1px solid #d3d3d3;
+    border-top: 1px solid #d3d3d3;
+    border-right: 1px solid #a6a6a6;
+    border-bottom: 1px solid #a6a6a6;
+    border-radius: 3px;
+}
+
+a.btn.minix.fa.fa-thumbs-up.fr {
+    color: #fff!important;
+    border-radius: 4px;
+    padding: 1px 4px 2px 5px;
+    margin-top: 6px;
+    background-repeat: repeat-x;
+    border-color: #b92c28;
+    background-image: linear-gradient(to bottom,#d9534f 0,#c12e2a 100%);
+    margin-left: 3px;
+}
+a.btn.minix.fa.fa-comment.fr {
+    color: #fff!important;
+    border-radius: 4px;
+    padding: 1px 4px 2px 5px;
+    margin-top: 6px;
+    background-repeat: repeat-x;
+    background-image: linear-gradient(to bottom,#c93091 0,#c93091 100%);
+}
+span.number {
+    display: none!important;
+}
+.btn.minix.btn-primary.fa.fa-times.fa {
+    display: inline !important;
+    background-color: #da303059;
+    border-radius: 33%;
+    border: #8dafb333 solid 1px !important;
+    margin-left: -3px;
+}
+.room.borderg.hand.nosel.fl .fa-microphone:before {
+    margin: 0 0 0 -18px;
+    font-size: 20px;
+    color: #ff0a0a;
+    -webkit-animation: mymove .5s infinite;
+    animation: mymove .5s infinite;
+}.room.borderg.hand.nosel.fl {
+    border-color: rgb(79 86 94 / 20%)!important;
+    background-color: #fff3fa!important;
+}
+.bord {
+    border-inline: 4px solid dodgerblue !important;
+}
+button.border.btn.label.label-success.fl.fa.fa-plus {
+    color: #ffffff !important;
+    font-family: jazeera, FontAwesome;
+    width: 100%;
+    border-radius: 0px;
+    border: 1px solid rgb(255 255 255);
+    content: " YAH";
+    text-align: center!important;
+}
+div#d2bc .uzr.fl.corner.borderg {
+    border-radius: 9px!important;
+    margin: 3px 1px -1px 2px !important;
+    border-color: #000;
+    width: 95%;
+    background: repeating-linear-gradient( 
+45deg,hsla(0,0%,100%,.8),#fff 100px,hsla(0,0%,100%,.6901960784313725) 0,rgba(255,246,246,.6) 200px),repeating-linear-gradient( 
+-45deg,hsla(0,0%,100%,.5),hsla(0,0%,100%,.93) 100px,hsla(0,3.8%,69%,.8196078431372549) 0,hsla(0,0%,100%,.9019607843137255) 200px);
+    color: #0f0202;
+    padding: 2px 1px;
+    left: 6px;
+    border-radius: 0 14px 0 14px;
+    box-shadow: 0 0 10px 0 #000;
+}
+.btn.minix.btn-primary.fa.fa-times.fa {
+    display: inline !important;
+    background-color: #da303059;
+    border-radius: 33%;
+    border: #8dafb333 solid 1px !important;
+    margin-left: -3px;
+}
+</style>`).insertBefore('body');
